7c784327504723cd9e9d7526dd5c0d5ae9943998
Madhu sudan <madhusudan@Madhus-MacBook-Air.local>
2026-02-12 17:28:17 +0530

    Updated frontend UI

--- hrms-frontend/src/main.jsx
+++ hrms-frontend/src/main.jsx
@@ -9,4 +9,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </React.StrictMode>
 );
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
